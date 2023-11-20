1f0b56d0d931e0f0c83e6f952f7d76edb1978f2f
armanmkrtchyan2005 <armanmkrtchyan.2005@mail.ru>
2023-11-21 00:05:52 +0400

    All algorithms without 4 and 10 algorithms

--- src/socket/socket.service.ts
+++ src/socket/socket.service.ts
@@ -1,3 +1,6 @@
+// 4.  -----------------------------?
+// 10. -----------------------------?
+
 import { WsException } from "@nestjs/websockets";
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
@@ -9,17 +12,15 @@ import { User, UserDocument } from "src/user/schemas/user.schema";
 import { BetDto } from "./dto/bet.dto";
 import { Bet, IBet } from "src/user/schemas/bet.schema";
 import { CashOutDto } from "./dto/cashOut.dto";
+import * as _ from "lodash";
 
 const STOP_DISABLE_MS = 2000;
 const LOADING_MS = 5000;
 const MAX_COEFF = 110;
+const HOUR_IN_MS = 1000 * 60 * 60;
 
 function sleep(ms: number = 0) {
   return new Promise(res => setTimeout(res, ms));
-}
-
-function random(min: number, max: number) {
-  return Math.random() * (max - min) + min;
 }
 
 @Injectable()
@@ -34,25 +35,44 @@ export class SocketService {
   private isBetWait = true;
 
   private interval: string | number | NodeJS.Timeout;
-  private random = random(0, MAX_COEFF);
+  private random = _.random(MAX_COEFF, true);
   // Algorithm 1
-  private threePlayers: UserDocument[] = [];
+  private threePlayers: IBet[] = [];
 
-  // Algorithm 2
-  private totalAmount: number = 0;
+  // Algorithm 2, Algorithm 8
   private maxWinAmount: number = 0;
+
+  // Algorithm 7
+  private totalWinsCount: number = 0;
+
+  // Algorithm 9
+  private randomCoeffs = [1, 1.2, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 
   handleConnection(socket: Socket): void {}
 
   handleDisconnect(socket: Socket): void {}
 
+  randomOneHourAlgorithm() {
+    const daley = _.random(HOUR_IN_MS, 2 * HOUR_IN_MS);
+    setTimeout(() => {
+      this.random = _.random(1, 100, true);
+      this.randomOneHourAlgorithm();
+    }, daley);
+  }
+
   handleStartGame() {
     this.interval = setInterval(() => this.game(), 20);
+    // 6. random one hour algorithm
+    this.randomOneHourAlgorithm();
   }
 
   async game() {
-    this.step += 0.0006;
     this.x += this.step;
+    this.step += 0.0006;
+    // console.log(this.x);
+
+    // 4. -?
+
     if (this.x > this.random) {
       await this.loading();
     }
@@ -113,7 +133,7 @@ export class SocketService {
 
     //1. 3 player algorithm in Cash Out
     if (true) {
-      this.threePlayers = this.threePlayers.filter(u => u._id !== user._id);
+      this.threePlayers = this.threePlayers.filter(u => u.player !== userPayload?.id);
       if (!this.threePlayers.length) {
         this.loading();
       }
@@ -127,14 +147,33 @@ export class SocketService {
       }
     }
 
+    // 7. Bet counts algorithm
+    if (false) {
+      const winsCount = this.currentPlayers.filter(bet => {
+        return bet.win;
+      }).length;
+
+      if (winsCount >= this.totalWinsCount) {
+        this.loading();
+      }
+    }
+
+    // 8. -------------
+
+    if (true) {
+      const totalWinAmount = this.currentPlayers.reduce((prev, next) => prev + next.win, 0);
+      if (totalWinAmount >= this.maxWinAmount) {
+        this.loading();
+      }
+    }
+
     return { message: "Вы выиграли" };
   }
 
   private async loading() {
     this.x = 1;
     this.step = 0.0006;
-    this.random = random(0, MAX_COEFF);
-    console.log(this.random);
+    this.random = _.random(MAX_COEFF, true);
 
     clearInterval(this.interval);
 
@@ -145,13 +184,6 @@ export class SocketService {
     this.currentPlayers = [];
     this.threePlayers = [];
 
-    //1. 3 player algorithm loading
-    if (true) {
-      const count = await this.userModel.count();
-      const randomCount = random(0, count);
-      this.threePlayers = await this.userModel.find().skip(randomCount).limit(3);
-    }
-
     await sleep(STOP_DISABLE_MS);
 
     this.isBetWait = false;
@@ -160,10 +192,45 @@ export class SocketService {
 
     await sleep(LOADING_MS);
 
+    //1. 3 player algorithm loading
+    if (true) {
+      this.threePlayers = _.sampleSize(this.threePlayers, 3);
+      this.random = _.random(110, true);
+    }
+
     // 2. Net income algorithm loading
     if (true) {
-      this.totalAmount = this.currentPlayers.reduce((prev, curr) => prev + curr.bet, 0);
-      this.maxWinAmount = (this.totalAmount * random(30, 100)) / 100;
+      const totalAmount = this.currentPlayers.reduce((prev, curr) => prev + curr.bet, 0);
+      this.maxWinAmount = (totalAmount * _.random(30, 100, true)) / 100;
+    }
+
+    // 3. 1-3 Coeff algorithm
+    if (true) {
+      this.random = _.random(1, 3);
+    }
+
+    // 5. 1-2 Coeff algorithm
+    if (false) {
+      this.random = _.random(1, 2, true);
+    }
+
+    // 7. Bet counts algorithm
+    if (false) {
+      this.random = _.random(5, 10, true);
+      this.totalWinsCount = (this.currentPlayers.length * _.random(20, 70, true)) / 100;
+    }
+
+    // 8. --------
+    if (false) {
+      const range = _.random(1, 3, true);
+      const totalAmount = this.currentPlayers.reduce((prev, curr) => prev + curr.bet, 0);
+      this.maxWinAmount = totalAmount * range;
+    }
+
+    // 9. ---------
+    if (true) {
+      const randomCoeff = _.sample(this.randomCoeffs);
+      this.random = _.random(randomCoeff - 0.2, randomCoeff - 0.01);
     }
 
     this.interval = setInterval(() => this.game(), 100);
